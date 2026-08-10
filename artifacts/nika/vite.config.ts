29a4f8a112828baaeaf400f640a1118e8a7d2c6b
Nevermind. <162158483+Diandrare@users.noreply.github.com>
2026-08-10 15:31:27 +0700

    -

--- artifacts/nika/vite.config.ts
+++ artifacts/nika/vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
